0c46f7cc7884e62e69f1bf52e3cb837fce8ae680
TarekJubaerS <joinshammoy@gmail.com>
2017-05-19 10:22:30 +0600

    Use config value instead

--- src/config.php
+++ src/config.php
@@ -7,10 +7,10 @@
  */
 return [
 // AMPQ Connection settigns for RabbitMQ
-	'amqp_host' => 'localhost',
-	'amqp_port' => 5672,
-	'amqp_user' => 'guest',
-	'amqp_pass' => 'guest',
-	'amqp_vhost' => '/',
-	'amqp_queue' => 'MyQueue'
+	'amqp_host' => env('RABBITMQ_HOST', 'localhost'),
+	'amqp_port' => env('RABBITMQ_PORT', 5672),
+	'amqp_user' => env('RABBITMQ_LOGIN', 'guest'),
+	'amqp_pass' => env('RABBITMQ_PASSWORD', 'guest'),
+	'amqp_vhost' => env('RABBITMQ_VHOST', '/'),
+	'amqp_queue' => env('RABBITMQ_QUEUE', 'MyQueue')
 ];
